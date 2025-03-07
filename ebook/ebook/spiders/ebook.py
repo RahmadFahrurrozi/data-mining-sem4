8fafa49050bd44faa46ee0aa08db63dd04fbcf00
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-03-07 11:06:03 +0700

    feat : sucsessfully get data and parse ke json file

--- ebook/ebook/spiders/ebook.py
+++ ebook/ebook/spiders/ebook.py
@@ -6,5 +6,17 @@ class EbookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        print("[ini data response]")
-        print(response)
+        print("[============================== START SCRAPING ==============================]")
+        # print(response.css('h3 a').get())
+        dataBuku = response.css('article')
+        for buku in dataBuku:
+            namaBuku = buku.css('h3 a::text').get()
+            hargaBuku = buku.css('p.price_color::text').get()
+            gambarBuku = buku.css('div.image_container img::attr(src)').get()
+            print(f"Nama Buku: {namaBuku},\nHarga Buku: {hargaBuku},\nGambar Buku: {gambarBuku}")
+            yield {
+                'namaBuku': namaBuku,
+                'hargaBuku': hargaBuku,
+                'gambarBuku': gambarBuku
+            }
+        print("[============================== END SCRAPING ==============================]")
